646e47f6945aebfa759ea3ca273a3d6f0c7d2b8c
TheJudgeMonkey <mr.stas.anufriev@gmail.com>
2021-08-03 18:19:14 +0300

    Edit user-frienship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :user_stocks
   has_many :stocks, through: :user_stocks, dependent: :destroy
   has_many :friendships
-  has_many :friends, through: :friendships
+  has_many :friends, through: :friendships, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
